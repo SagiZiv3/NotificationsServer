2979f8ce31ab40be06de90dd4de69e031820faa9
Sagi Ziv <73391391+SagiZiv3@users.noreply.github.com>
2025-10-25 00:13:05 +0300

    Updated main.py to a working example. It uses all the new packages and exposes a '/notify' POST endpoint that gets a sequence of NotificationProcessor from the DI container.
    Each NotificationProcessor gets a instance of a configuration relevant to it.

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from contextlib import asynccontextmanager
-import uvicorn
 import asyncio
-import os
+from collections import Counter
+from collections.abc import Sequence
 
+from fastapi.responses import JSONResponse
+
+from dependency_injection import DependencyContainerBuilder
 from models import Notification
-from notification_processors import load_notification_processors, NotificationProcessor
+from notification_processors import load_notification_processors, NotificationProcessor, ProcessingResult, \
+    TelegramConfig, GotifyConfig
+from web_application import WebApplicationBuilder, WebApplication
 
 
-
-
-processors: list[NotificationProcessor] = []
-deps: DependenciesHandler = DependenciesHandler()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup phase
-    global processors
-    processors = load_notification_processors()
-    yield
-    # Shutdown phase (optional cleanup)
-    print("Shutting down notification server...")
-
-app = FastAPI(lifespan=lifespan)
-
-@app.post("/notify")
-async def receive_notification(notification: Notification):
-    # Dispatch notification to all processors concurrently
-    await asyncio.gather(*(p.process(notification) for p in processors))
+async def process_notification(notification: Notification, processors: tuple[NotificationProcessor]):
+    results: Sequence[ProcessingResult] = await asyncio.gather(*(p.process(notification) for p in processors))
+    counter = Counter(results)
 
     return JSONResponse(
-        content={"status": "forwarded", "processors": [p.__class__.__name__ for p in processors]},
+        content={"status": f"forwarded '{notification.title}'",
+                 "processors": [p.__class__.__name__ for p in processors],
+                 "counter": tuple(map(lambda e: e.name, counter.elements())),
+                 "fail": counter.get(ProcessingResult.Fail, 0),
+                 "skip": counter.get(ProcessingResult.Skip, 0),
+                 "success": counter.get(ProcessingResult.Success, 0)},
         status_code=200,
     )
 
 
+def add_notification_processors_to_di(di: DependencyContainerBuilder):
+    processors = load_notification_processors()
+    for t in processors:
+        di.add_scoped(NotificationProcessor, t)
+
+
+from configuration import data_providers
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    wab: WebApplicationBuilder = WebApplicationBuilder()
+    add_notification_processors_to_di(wab.services)
+
+    wab.configuration \
+        .add_provider(data_providers.dot_env_file())
+    section = wab.configuration.get_section('Gotify')
+    wab.configuration.configure(section, GotifyConfig)
+    section = wab.configuration.get_section('Telegram')
+    wab.configuration.configure(section, TelegramConfig)
+
+    app: WebApplication = wab.build()
+    app.map_post('/notify', process_notification) \
+        .with_dependencies() \
+        .apply()
+    app.run()
